2f8c1c8751d62b032ef7063bc2662730c109dd02
kyle <noel8331@live.com>
2020-10-06 23:13:26 -0500

    Excercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,5 @@
 "use strict";
+
 window.onload = function () {
   var board = document.getElementById("board").getElementsByTagName("div");
   var dict = {};
@@ -8,17 +9,45 @@ window.onload = function () {
   function Process(square) {
     for (var key in dict) {
       if (square == dict[key]) {
-        console.log(boardState);
         boardState[key] = turn;
 
         if (turn == 1) {
           square.classList.add("X");
           square.innerHTML = "X";
-
-          turn = 2;
         } else {
           square.classList.add("O");
           square.innerHTML = "O";
+        }
+        // prettier-ignore
+        if (
+          boardState[0] == turn && boardState[1] == turn && boardState[2] == turn ||
+          boardState[3] == turn && boardState[4] == turn && boardState[5] == turn ||
+          boardState[6] == turn && boardState[7] == turn && boardState[8] == turn ||
+          boardState[0] == turn && boardState[3] == turn && boardState[6] == turn ||
+          boardState[1] == turn && boardState[4] == turn && boardState[7] == turn ||
+          boardState[2] == turn && boardState[5] == turn && boardState[8] == turn ||
+          boardState[0] == turn && boardState[4] == turn && boardState[8] == turn ||
+          boardState[2] == turn && boardState[4] == turn && boardState[6] == turn
+      ) {
+          
+          document.getElementById("status").classList.add("you-won");
+
+          if (turn == 1) {
+              document
+                  .getElementById("status")
+                  .innerHTML = "Congratulations! X is the Winner!";
+
+          } else {
+              document
+                  .getElementById("status")
+                  .innerHTML = "Congratulations! O is the Winner!";
+
+          }
+        
+      }
+        if (turn == 1) {
+          turn = 2;
+        } else {
           turn = 1;
         }
       }
